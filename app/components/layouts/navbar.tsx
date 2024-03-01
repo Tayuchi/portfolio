02402ad32fc707090ace630a4ec0c6b7c56a7d03
Tayu <yukitake844@gmail.com>
2024-03-01 23:35:44 +0900

    page setup

---
+++ app/components/layouts/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="fixed w-full bg-white z-10 shadow-sm">
+      nav bar
+    </nav>
+  )
+}
+
+export default Navbar
