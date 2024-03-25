facab1a8f3146592ba4704147888144fea4f3046
Tayu <yukitake844@gmail.com>
2024-03-25 23:42:46 +0900

    layout

---
+++ components/layouts/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className='flex flex-col items-center justify-between p-10'>
+        &copy;2024 Tayu 
+      </div>
+    )
+  }
+
+export default Footer;
---
+++ components/layouts/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+      <nav className="divide-y border-gray-200 dark:border-gray-800 border-b fixed w-full top-0 bg-white dark:bg-gray-800 z-10">
+        <div className="px-4 py-3 md:py-6 lg:px-6">
+          <div className="flex items-center space-y-2 md:space-y-0 md:space-x-6">
+            <Link href="/" className="text-2xl font-bold tracking-tighter mr-4">
+              Tayu
+            </Link>
+            <nav className="flex items-center space-x-6 text-sm">
+              <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/"
+              >
+                Top
+              </Link>
+              <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/about"
+              >
+                About
+              </Link>
+              <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/about"
+              >
+                Works
+              </Link>
+              <Link
+                className="font-medium text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href="/bbs-posts/create"
+              >
+                Contact
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+
+export default Navbar;
