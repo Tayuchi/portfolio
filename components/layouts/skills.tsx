640d3873afc7605007d701b38f6a0f2d29d7dff4
Tayu <yukitake844@gmail.com>
2024-04-23 21:36:46 +0900

    added packages

--- components/layouts/skills.tsx
+++ components/layouts/skills.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Image from 'next/image';
+// import { Html5 } from 'devicon-react-svg'
+ 
 const Skills = () => {
   return (
     <div className='flex flex-col items-center justify-between p-20'>
-        <h1 className="text-2xl font-semibold text-center">Skills</h1>
+      <h1 className="text-2xl font-semibold text-center">Skills</h1>
     </div>
   )
 }
