426857c9d8ecc6d62d66adb2161ba484ed5b8b9d
Tayu <yukitake844@gmail.com>
2024-05-14 09:11:42 +0900

    added skills section

--- components/layouts/skills.tsx
+++ components/layouts/skills.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Image from 'next/image';
+import { Card, CardContent, CardTitle } from '../ui/card';
  
 const Skills = () => {
   return (
-    <div className='flex flex-col items-center justify-between m-20'>
+    <div className='lg:w-[30%] md:w-[60%] sm:w-[80%] mx-auto mb-20'>
       <h1 className="text-2xl font-semibold text-center">Skills</h1>
+      <Card className='card mx-auto flex pt-10 pb-10'>
+        <CardTitle >Tech Stack</CardTitle>
+        <CardTitle >Qualifications</CardTitle>
+        <CardContent>
 
+        </CardContent>
+      </Card>
     </div>
   )
 }
