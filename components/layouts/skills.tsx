0efbd9cae9f68eda6a45f5e16ee674a21aab9ff5
takechi808 <yukitake844@gmail.com>
2024-05-14 15:53:43 +0900

    added files

--- components/layouts/skills.tsx
+++ components/layouts/skills.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardTitle } from '../ui/card';
 const Skills = () => {
   return (
     <div className='lg:w-[30%] md:w-[60%] sm:w-[80%] mx-auto mb-20'>
-      <h1 className="text-2xl font-semibold text-center">Skills</h1>
+      <h1 className="text-2xl font-semibold text-center mb-5">Skills</h1>
       <Card className='card mx-auto flex flex-col items-center pt-10 pb-10'>
         <CardTitle >Tech Stack</CardTitle>
         <CardContent>
@@ -14,7 +14,7 @@ const Skills = () => {
         <CardTitle >Qualifications</CardTitle>
         <CardContent>
           <ul>
-            <li>TOEIC L&R 935</li>
+            <li>TOEIC L&R score 935</li>
             <li>実用英語技能検定1級</li>
             <li>ITパスポート</li>
             <li>基本情報技術者試験</li>
