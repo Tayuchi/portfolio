74b36cb8cac7b4dfbbf5d459e028e38f137a9e7c
takechi808 <yukitake844@gmail.com>
2024-05-15 14:55:55 +0900

    changes to be committed

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons"
 
-const About = () => {
+const AboutCard = () => {
   const [isFlipped, setIsFlipped] = useState(false)
   const [isAnimating, setIsAnimating] = useState(false)
 
@@ -69,4 +69,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default AboutCard;
