de83b3ceffd4ad8bfbc12c44955017180ddc4b13
Tayu <yukitake844@gmail.com>
2024-05-18 22:25:19 +0900

    added icons

--- components/layouts/skills.tsx
+++ components/layouts/skills.tsx
@@ -8,17 +8,21 @@ const Skills = () => {
       <h1 className="text-2xl font-semibold text-center mb-5">Skills</h1>
       <Card className='card mx-auto flex flex-col items-center pt-10 space-y-5 pb-5'>
         <CardTitle >Tech Stack</CardTitle>
-        <CardContent className='mx-auto flex flex-wrap items-center'>
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
-          <Image src='/skills/icons8-css-144.png' width={50} height={300} alt='css' />
+        <CardContent className='mx-auto flex flex-wrap items-center space-x-2 m-10 '>
+          <Image src='/skills/icons8-html-5-500.png' width={50} height={300} alt='html' />
+          <Image src='/skills/icons8-css3-500.png' width={50} height={300} alt='css' />
+          <Image src='/skills/icons8-javascript-500.png' width={50} height={300} alt='javascript' />
+          <Image src='/skills/icons8-typescript-500.png' width={50} height={300} alt='typescript' />
+          <Image src='/skills/icons8-python-480.png' width={50} height={300} alt='python' />
+          <Image src='/skills/icons8-django-500.png' width={50} height={300} alt='django' />
+          <Image src='/skills/react.png' width={50} height={300} alt='react' />
+          <Image src='/skills/next.png' width={50} height={300} alt='next' />
+          <Image src='/skills/prisma.png' width={50} height={300} alt='prisma' />
+          <Image src='/skills/vercel.png' width={50} height={300} alt='vercel' />
+          <Image src='/skills/heroku.png' width={50} height={300} alt='vercel' />
+          <Image src='/skills/render.png' width={50} height={300} alt='vercel' />
+          <Image src='/skills/icons8-docker-500.png' width={50} height={300} alt='docker' />
+          <Image src='/skills/icons8-github-500.png' width={50} height={300} alt='github' />
         </CardContent>
         <CardTitle>Licenses</CardTitle>
         <CardContent className='space-y-5'>
